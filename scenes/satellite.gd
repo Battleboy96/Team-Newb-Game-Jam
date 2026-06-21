7efa53b607baaad1ffd5fff48a4d975c198ee860
Hikiru <hikiruu@proton.me>
2026-06-21 16:52:10 -0500

    Placeholder assets, basic movement and gravity

--- scenes/satellite.gd
+++ scenes/satellite.gd
@@ -1,40 +1,26 @@
 extends CharacterBody2D
 
-@export var mass: float = 1
-@export var parent: ParentBody
-@export var motion: Vector2 = Vector2(0, 0)
+var rotation_speed := 2.0
+var speed := 1.0
+var terminal_velocity := 5.0
+var gravity_strength := 1.0
 
-@export var al: float # inverse of semi-major axis 
-#(to make hyperbolic orbits not div by zero error)
-@export var e: float # eccentricity
-@export var w: float # argument of periapsis
+func _process(delta: float) -> void:
+	movement(delta)
+	gravity(delta)
 
-@onready var G: float = 1 #gravitational constant
-@onready var mu: float = G * parent.mass
+func movement(delta: float) -> void:
+	if Input.is_action_pressed("left"):
+		rotation -= rotation_speed * delta
+	if Input.is_action_pressed("right"):
+		rotation += rotation_speed * delta
+	if Input.is_action_pressed("boost"):
+		velocity += transform.y * speed * delta
+		velocity.x = clamp(velocity.x, -terminal_velocity, terminal_velocity)
+		velocity.y = clamp(velocity.y, -terminal_velocity, terminal_velocity)
+		print(velocity)
+	position -= velocity
 
-@onready var rel_pos: Vector2 = global_position - parent.global_position
-
-func _physics_process(delta: float) -> void:
-	calc_orbit()
-	prediction_problem(delta)
-
-
-func calc_orbit():
-	rel_pos = global_position - parent.global_position # position relative to parent
-	var r: float = rel_pos.length() # current radius from parent
-	var v2: float = velocity.length_squared() # (use in next eq)
-	var e_vec: Vector2 = (1/mu)*(((v2 - (mu/r))*rel_pos) - (velocity*(rel_pos.dot(velocity)))) # eccentricity vector
-	e = e_vec.length()
-	var h: float = velocity.cross(rel_pos) # angular momentum vector
-	var p: float = h**2 / mu # semi latus rectum (probably unnecessary)
-	al = ((2*mu)/(r-v2)) / mu
-	w = (Vector2(1,0).dot(e_vec)) / (e_vec.length())
-
-
-func prediction_problem(delta: float):
-	# we got uhhhhh vectors v1 (velocity), r1 (rel_pos), dt (delta), and parameters
-	# find vectors v2 r2 then return them
-	# then main _physics_process can move to new r2 and update v2??
-	# not move_and_slide. that wont be accurate enough. probably
-	
-	pass
+func gravity(delta: float) -> void:
+	var gravity_direction: Vector2 = (%Planet.global_position - self.global_position).normalized()
+	velocity -= gravity_direction * gravity_strength * delta
